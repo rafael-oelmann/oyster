bb12c415ac329c2394b97071c608eb280356bb16
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-11-02 15:54:40 +0000

    Exercise 5 - top up method added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,17 @@
-require "oystercard"
+require 'oystercard'
 
 describe Oystercard do
-
-  it "when created has a balance of 0" do
-    expect(subject.balance).to eq 0 
+  context '#balance' do
+    it 'when created has a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
   end
 
+  context '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance of the card' do
+      expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+    end
+  end
 end
