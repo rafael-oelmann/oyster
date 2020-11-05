0c514748e19b142c383e30251a5c2294a3727898
Rafael <rafael.oelmann@gmail.com>
2020-11-05 15:48:05 +0000

    journeys can now store an array of hashes (entry and exit stations)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,18 +1,16 @@
 class Oystercard
-  attr_reader :balance, :entry_station, :exit_station, :journeys
+  attr_reader :balance, :entry_station, :journeys
   LIMIT = 90
   MIN_FARE = 1
 
   def initialize(balance = 0)
     @balance = balance
-    @in_journey = false
     @entry_station = nil
-    @exit_station = nil
-    @journeys = {}
+    @journeys = []
   end
 
   def in_journey?
-    !!entry_station
+    !!@entry_station 
   end
 
   def top_up(amount)
@@ -23,17 +21,24 @@ class Oystercard
 
   def touch_in(entry_station)
     raise "not enough balance" if balance < MIN_FARE
-    @in_journey = true
-    @entry_station = entry_station
+    update_entry_station(entry_station)
   end
 
   def touch_out(exit_station)
     deduct(MIN_FARE)
-    @entry_station = nil
-    @exit_station = exit_station
+    update_exit_station(exit_station)
   end
 
   private
+  def update_entry_station(entry_station)
+    @entry_station = entry_station
+    @journeys << {entry_station: entry_station}
+  end
+
+  def update_exit_station(exit_station)
+    @entry_station = nil
+    @journeys.last[:exit_station] = exit_station
+  end
 
   def deduct(amount)
     @balance -= amount
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,12 @@ require 'oystercard'
 
 describe Oystercard do
 
+  let(:entry_station) { double :entry_station }
+  let(:exit_station) { double :exit_station }
+  let(:topped_up_card) { Oystercard.new(5)} 
+  let(:card_in_journey) { Oystercard.new(5).touch_in(entry_station) }
+  let(:journey){ {entry_station: entry_station, exit_station: exit_station} }
+
   context '#Initialized Card' do
     it 'when created has a balance of 0' do
       expect(subject.balance).to eq 0
@@ -35,11 +41,6 @@ describe Oystercard do
   end
 
   context '#Touching in/out' do
-
-    let(:entry_station) { double :entry_station }
-    let(:exit_station) { double :exit_station }
-    let(:topped_up_card) { Oystercard.new(5)} 
-    let(:card_in_journey) { Oystercard.new(5).touch_in(entry_station) }
     
 
     it { is_expected.to respond_to :touch_in }
@@ -49,28 +50,45 @@ describe Oystercard do
     end
 
     it 'touching in will change status of in_journey to true' do
-      topped_up_card.touch_in(entry_station)
-      expect(topped_up_card).to be_in_journey
+      subject.top_up(5)
+      subject.touch_in(entry_station)
+      expect(subject).to be_in_journey
     end
 
     it 'touching out in will change status of in_journey to false' do
-      topped_up_card.touch_out(exit_station)
-      expect(topped_up_card).not_to be_in_journey
+      subject.top_up(5)
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject).not_to be_in_journey
     end
 
     it 'will charge minimum fare deducting it from balance' do
-      expect { topped_up_card.touch_out(exit_station) }.to change { topped_up_card.balance }.by(-Oystercard::MIN_FARE)
+      subject.top_up(5)
+      subject.touch_in(entry_station)      
+      expect { subject.touch_out(exit_station) }.to change { subject.balance }.by(-Oystercard::MIN_FARE)
     end
 
     it 'stores the entry station' do 
-      expect(card_in_journey).to eq entry_station  
+      topped_up_card.touch_in(entry_station)
+      expect(topped_up_card.journeys[0][:entry_station]).to eq entry_station 
     end
 
     it 'stores the exit_station' do 
       topped_up_card.touch_in(entry_station)
       topped_up_card.touch_out(exit_station)
-      expect(topped_up_card.exit_station).to eq exit_station
+      expect(topped_up_card.journeys[-1][:exit_station]).to eq exit_station
     end
 
+  end
+
+  context "#journey" do 
+    
+    it 'store a journey' do 
+      topped_up_card.touch_in(entry_station)
+      topped_up_card.touch_out(exit_station)
+      expect(topped_up_card.journeys).to include journey
+    end 
+
+  
   end
 end
